bc28b9132f98de8a3a0c1a40b82e16591f81528c
Courage <yh3290@columbia.edu>
2021-06-30 14:51:51 +0800

    [Project] Start project workstation and add readme log

---
+++ java_project/README.md
@@ -0,0 +1,4 @@
+This is a JAVA mini-project workstation for practice and fun.
+
+2021/06/30
+
---
+++ kick/Round_A_2021_1/Solution.java
@@ -0,0 +1,24 @@
+package Round_A_2021_1;
+// K Goodness String, reference: https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068cca3
+import java.util.*;
+import java.io.*;
+public class Solution{
+    public static void main(String[] args){
+        try
+        {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            int case_number = Integer.parseInt(bf.readLine());
+            for (int k = 0; k < case_number; ++k)
+            {
+                System.out.print("Case #"+(k+1)+": ");
+                // TBD
+                System.out.println("");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return;
+        }
+    }
+}
