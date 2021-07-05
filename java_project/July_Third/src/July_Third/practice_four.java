994854f76ae16f67c6545b9667334a0276858720
Courage <yh3290@columbia.edu>
2021-07-05 14:33:20 +0800

    [Project] practice jar packet and import

--- java_project/July_Third/src/July_Third/practice_four.java
+++ java_project/July_Third/src/July_Third/practice_four.java
@@ -8,8 +8,25 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class practice_four {
-	public static void main (String[] args) {
+public class practice_four extends JFrame{
+	private JPanel jp1;
+	private JPanel jp2;
+	practice_four() {
+		jp1 = new JPanel(new BorderLayout());
+		jp2 = new JPanel(new BorderLayout());
+		jp1.add(new JLabel("salfdjalsdkf"), BorderLayout.CENTER);
+		jp2.add(new JButton(), BorderLayout.CENTER);
+		add(jp1, BorderLayout.WEST);
+		add(jp2, BorderLayout.EAST);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 400); // 设置窗口的大小
+		setVisible(true);
+	}
+	private void setGUI()
+	{
 		
+	}
+	public static void main (String[] args) {
+		new practice_four();
 	}
 }
