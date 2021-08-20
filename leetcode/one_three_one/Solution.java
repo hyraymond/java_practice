dc659ed20a5b059511f424a7a874a7355dcbe540
Courage <yh3290@columbia.edu>
2021-08-20 11:50:01 +0800

    [Leetcode] Puzzle 131, 分割回文串

---
+++ leetcode/one_three_one/Solution.java
@@ -0,0 +1,51 @@
+package one_three_one;
+
+import java.util.*;
+class Solution {
+    public List<List<String>> partition(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if ((j==0) || (i-j >= 0 && i + j < n && s.charAt(i-j) == s.charAt(i+j) && dp[i-j+1][i+j-1] == true))
+                {
+                    dp[i-j][i+j] = true;
+                }
+
+                if (i + 1 < n && s.charAt(i) == s.charAt(i + 1))
+                {
+                    if ((j==0) || (i-j >= 0 && i + j + 1 < n && s.charAt(i-j) == s.charAt(i+j + 1) && dp[i-j+1][i+j] == true))
+                    {
+                        dp[i-j][i+j+1] = true;
+                    }
+                }
+            }
+        }
+        List<List<String>> result = new ArrayList<List<String>>();
+        List<String> temp = new ArrayList<String>();
+        check(s, result, temp, dp, 0);
+        return result;
+    }
+    private void check(String s, List<List<String>> result, List<String> temp, boolean[][] dp, int index)
+    {
+        int n = s.length();
+        if (index == n)
+        {
+            result.add(new ArrayList<String>() {{
+                addAll(temp);
+            }});
+        }
+        for (int i = index; i < n; ++i)
+        {
+            if (dp[index][i] == true)
+            {
+                temp.add(s.substring(index, i+1));
+            }
+            check(s, result, temp, dp, i+1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
--- leetcode/one_three_three_nine/Solution.java
+++ leetcode/one_three_three_nine/Solution.java
@@ -16,43 +16,43 @@ import java.util.*;
  *     }
  * }
  */
-class Solution {
-    int c = 1000000007;
-    long sum = 0;
-    long best = 0;
-    public int maxProduct(TreeNode root) {
+// class Solution {
+//     int c = 1000000007;
+//     long sum = 0;
+//     long best = 0;
+//     public int maxProduct(TreeNode root) {
         
-        if (root == null)
-        {
-            return 0;
-        }
+//         if (root == null)
+//         {
+//             return 0;
+//         }
 
-        dfs(root);
-        dfs2(root);
-        return (int)((best * (sum - best))%c);
-    }
-    private void dfs (TreeNode root)
-    {
-        if (null == root)
-        {
-            return;
-        }
-        sum += (long)root.val;
-        dfs(root.left);
-        dfs(root.right);
-    }
-    private long dfs2 (TreeNode root)
-    {
-        if (root == null)
-        {
-            return 0;
-        }
+//         dfs(root);
+//         dfs2(root);
+//         return (int)((best * (sum - best))%c);
+//     }
+//     private void dfs (TreeNode root)
+//     {
+//         if (null == root)
+//         {
+//             return;
+//         }
+//         sum += (long)root.val;
+//         dfs(root.left);
+//         dfs(root.right);
+//     }
+//     private long dfs2 (TreeNode root)
+//     {
+//         if (root == null)
+//         {
+//             return 0;
+//         }
 
-        long temp = (long)root.val + dfs2(root.left) + dfs2(root.right);
-        if (Math.abs(2 * temp - sum) < Math.abs(2 * best - sum))
-        {
-            best = temp;
-        }
-        return temp;
-    }
-}
+//         long temp = (long)root.val + dfs2(root.left) + dfs2(root.right);
+//         if (Math.abs(2 * temp - sum) < Math.abs(2 * best - sum))
+//         {
+//             best = temp;
+//         }
+//         return temp;
+//     }
+// }
