6189fb0dbfd3798645e6ccc3cdb76fad5c0c3571
Courage <yh3290@columbia.edu>
2021-08-21 17:29:02 +0800

    [Kick] update the template text

---
+++ kick/Main/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            // String[] str = input.nextLine().split(" ");
+            // int n = str.length;
+            // int[] num = new int[n];
+            int result = input.nextInt();
+            System.out.println(result);
+        }
+    }
+}
