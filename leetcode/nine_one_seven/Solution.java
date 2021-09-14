478db03be28f709baf7113a3ea2f16f1f2151b50
Courage <yh3290@columbia.edu>
2021-09-14 13:38:51 -0400

    [Leetcode] Puzzle 917, Reverse Only Letters

---
+++ leetcode/nine_one_seven/Solution.java
@@ -0,0 +1,29 @@
+package nine_one_seven;
+import java.util.*;
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        StringBuilder str = new StringBuilder(s);
+        while(start < end)
+        {
+            if (!Character.isAlphabetic(s.charAt(start)))
+            {
+                ++start;
+            }
+            else if (!Character.isAlphabetic(s.charAt(end)))
+            {
+                --end;
+            }
+            else
+            {
+                char temp = str.charAt(start);
+                str.insert(start, str.charAt(end));
+                str.deleteCharAt(start);
+                str.insert(end, temp);
+                str.deleteCharAt(end);
+            }
+        }
+        return str.toString();
+    }
+}
