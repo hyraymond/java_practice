280995aee777527d4071c0854676509ae4810c1b
hyraymond <yh3290@columbia.edu>
2021-09-28 16:13:47 -0400

    [Leetcode] Puzzle 112, Path Sum

---
+++ leetcode/one_one_two/Solution.java
@@ -0,0 +1,40 @@
+package one_one_two;
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean dfs(TreeNode root, int targetSum, int temp)
+    {
+        if (root == null)
+        {
+            return false;
+        }
+
+        temp += root.val;
+        if (root.left == null && root.right == null)
+        {
+            return temp == targetSum;
+        }
+        else
+        {
+            return dfs(root.left, targetSum, temp) || dfs(root.right, targetSum, temp);
+        }
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return dfs(root, targetSum, 0);
+    }
+}
