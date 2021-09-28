b6e36456f7e92e22f8c8d620add2dff03babbbd0
hyraymond <yh3290@columbia.edu>
2021-09-28 00:28:14 -0400

    [CodeForce] Stone Lifting

---
+++ leetcode/codeforce_stone_lifting/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext())
+        {
+            int key = in.nextInt();
+            int result = 0;
+            while (key > 0)
+            {
+                int temp = key&1;
+                if (temp == 1)
+                {
+                    ++result;
+                }
+                key = key >> 1;
+            }
+            System.out.println(result);
+        }
+    }
+}
