1e7a9e9e7c4a8ab89edfd5d901c8ef77d2d09a91
hyraymond <yh3290@columbia.edu>
2021-10-10 14:07:29 -0400

    [Leetcode] Puzzle 324, Wiggle Sort two

--- leetcode/two_eight_zero/Solution.java
+++ leetcode/two_eight_zero/Solution.java
@@ -3,18 +3,25 @@ import java.util.*;
 class Solution {
     public void wiggleSort(int[] nums) {
         int n = nums.length;
+        int mid = (n-1)/2;
+        int n1 = mid + 1;
+        int n2 = n - n1;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
         Arrays.sort(nums);
-        int[] result = new int[n];
-        int start = 0;
-        int end = n - 1;
-        int i = 0;
-        while(i < n){
-            result[i++] = nums[start++];
-            if (i < n) result[i++] = nums[end--];
+        int i = n-1;
+        for (int j = 0; j < n2; ++j){
+            right[j] = nums[i--];
         }
-
-        for (int j = 0; j < n; ++j){
-            nums[j] = result[j];
+        for (int j = 0; j < n1; ++j){
+            left[j] = nums[i--];
+        }
+        i = 0;
+        int p1 = 0;
+        int p2 = 0;
+        while (i < n){
+            nums[i++] = left[p1++];
+            if (i < n) nums[i++] = right[p2++];
         }
     }
 }
